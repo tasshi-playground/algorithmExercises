9c59a38e430683e5d6e00c4eab08df52643956b1
Masaharu TASHIRO <masatsr.kit@gmail.com>
2019-04-13 04:04:25 +0900

    [Add] main.go

---
+++ main.go
@@ -0,0 +1,14 @@
+/*!
+ * main.go
+ * binarySearchTree
+ *
+ * Copyright (c) 2019 Masaharu TASHIRO
+ * Released under the MIT license.
+ * see https://opensource.org/licenses/MIT
+ */
+
+package main
+
+func main() {
+
+}
