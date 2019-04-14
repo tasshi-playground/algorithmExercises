db7bf72fa830a4fa04e3f70479a6b2faf04c1fc1
Masaharu TASHIRO <masatsr.kit@gmail.com>
2019-04-14 17:40:22 +0900

    [Add] func parseArgs

--- main.go
+++ main.go
@@ -9,6 +9,43 @@
 
 package main
 
-func main() {
+import (
+	"errors"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+)
 
+func main() {
+	// Parse arguments
+	n, err := parseArgs(os.Args)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	fmt.Printf("N: %d\n", n)
+}
+
+func parseArgs(args []string) (int, error) {
+
+	if len(args) < 2 {
+		err := errors.New("Too few arguments")
+		return 0, err
+	} else if len(args) > 2 {
+		err := errors.New("Too much arguments")
+		return 0, err
+	}
+	n, err := strconv.Atoi(args[1])
+	if err != nil {
+		log.Println(err)
+		return 0, err
+	}
+	if n < 1 {
+		err := errors.New("N must be greater than 0")
+		return 0, err
+	}
+
+	return n, nil
 }
