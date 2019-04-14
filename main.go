5b74e5cab5f158a7f08cbcc062e915f3290e000e
Masaharu TASHIRO <masatsr.kit@gmail.com>
2019-04-14 14:42:27 +0900

    [Add] (tpl) func main()

--- main.go
+++ main.go
@@ -23,7 +23,53 @@ type Node struct {
 	right *Node
 	value int
 }
+
 func main() {
+	// Parse arguments
+	if len(os.Args) < 3 {
+		log.Println("Too few arguments")
+		return
+	} else if len(os.Args) > 3 {
+		log.Println("Too much arguments")
+		return
+	}
+	n, err := strconv.Atoi(os.Args[1])
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	if n < 1 {
+		fmt.Println("N must be greater than 0")
+		return
+	}
+
+	target, err := strconv.Atoi(os.Args[2])
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	if target < 1 {
+		fmt.Println("TargetValue must be greater than 0")
+		return
+	}
+
+	// Initilize list
+	rand.Seed(time.Now().UnixNano())
+	tree := insertRandomValues(n)
+
+	// Print unsorted list
+	fmt.Println("Tree")
+	tree.print()
+
+	// searching
+	searchedNode := tree.search(5)
+
+	// Print search result
+	fmt.Println("Search result")
+	searchedNode.print()
+
+}
+
 func (tree *Node) insert(value int) {
 
 }
