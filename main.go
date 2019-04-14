6656571a601ebe35b49a14619aeeed0d7d11a296
Masaharu TASHIRO <masatsr.kit@gmail.com>
2019-04-14 14:34:50 +0900

    [Add] define Node

--- main.go
+++ main.go
@@ -6,9 +6,23 @@
  * Released under the MIT license.
  * see https://opensource.org/licenses/MIT
  */
-
 package main
 
+import (
+	"fmt"
+	"log"
+	"math/rand"
+	"os"
+	"strconv"
+	"time"
+)
+
+// Node ...
+type Node struct {
+	left  *Node
+	right *Node
+	value int
+}
 func main() {
 
 }
